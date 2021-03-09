e90fb4f2498560a811d20a12da0a484be4ee9288
ArChanDDD <povla13@gmail.com>
2021-03-10 01:02:46 +0300

    1

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import telebot
-import time
 
 bot = telebot.TeleBot('1583853458:AAGKFi8qgnmWNZYrnCBySoTVE51A5lB3KNU')
+keyboard_start_tomat = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard_start_tomat.row('Начнем!')
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.from_user.id, '''Привет! Я бот, созданный помочь тебе устроить свое время так, чтобы ты смог 
-    работать максимально продуктивно. Пока что я умею только запускать 10-минутные томаты. Попробуем?''')
+    bot.send_message(message.from_user.id, '''
+    Привет! 
+    Я бот, созданный помочь тебе устроить свое время так, чтобы ты смог работать максимально продуктивно. 
+    Пока что я умею только запускать 10-минутные томаты. Попробуем?
+    ''', reply_markup=keyboard_start_tomat)
 
 
 @bot.message_handler(content_types=['text'])
