862a0f3681dce752ee880dd221131d2e7f3c673a
ArChanDDD <povla13@gmail.com>
2021-03-10 00:42:11 +0300

    1

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import telebot
+import time
 
 bot = telebot.TeleBot('1583853458:AAGKFi8qgnmWNZYrnCBySoTVE51A5lB3KNU')
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, f'Shalom')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, '''Привет! Я бот, созданный помочь тебе устроить свое время так, чтобы ты смог 
+    работать максимально продуктивно. Пока что я умею только запускать 10-минутные томаты. Попробуем?''')
 
 
 @bot.message_handler(content_types=['text'])
