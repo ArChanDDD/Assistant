68554594442472b67e1031706c2b0ed3a06da6ee
ArChanDDD <povla13@gmail.com>
2021-03-10 00:52:14 +0300

    1

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ bot = telebot.TeleBot('1583853458:AAGKFi8qgnmWNZYrnCBySoTVE51A5lB3KNU')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, '''Привет! Я бот, созданный помочь тебе устроить свое время так, чтобы ты смог 
+    bot.send_message(message.from_user.id, '''Привет! Я бот, созданный помочь тебе устроить свое время так, чтобы ты смог 
     работать максимально продуктивно. Пока что я умею только запускать 10-минутные томаты. Попробуем?''')
-    bot.send_message(message.chat.id, 'Будет весело!')
 
 
 @bot.message_handler(content_types=['text'])
